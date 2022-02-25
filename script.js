661dcc1db1aa3ba244eecac639ded3b63b4ee726
marcovdss <wintermarck@outlook.com>
2022-02-25 16:37:03 -0300

    fix lint

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function entrar() {
-    const email = document.querySelector('#login-email').value;
-    const senha = document.querySelector('#login-senha').value;
-    if (email === 'tryber@teste.com' && senha === '123456') {
-        alert('Olá, Tryber!');
-    } else {
-        alert('Email ou senha inválidos.');
-    }
+  const email = document.querySelector('#login-email').value;
+  const senha = document.querySelector('#login-senha').value;
+  if (email === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
 
-
 function configurar() {
-    document.querySelector('#botao-entrar').addEventListener('click', entrar);
-
+  document.querySelector('#botao-entrar').addEventListener('click', entrar);
 }
 
 window.onload = configurar;
